64fefc011161f5097fe8b798742d0d63ea5e74b4
rodolfoorlandajr <16815140144@ncr2.deped.gov.ph>
2026-04-28 11:10:11 +0800

    Fix frontend

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  env: {
+    PUBLIC_API_URL: process.env.PUBLIC_API_URL || "",
+  },
   async rewrites() {
     return [
       {
